157ac42438f470cfca9eaeb15c1c4207cc2b5f08
Alexander Yaroshevich <vozman@yandex.ru>
2018-03-29 23:52:34 +0300

    create xml write script

--- utils/ASAP_xml.py
+++ utils/ASAP_xml.py
@@ -1,8 +1,8 @@
-from xml.etree import ElementTree
+from xml.etree import cElementTree as ET
 
 
 def read_polygons_xml(xml_path):
-    root = ElementTree.parse(xml_path).getroot()
+    root = ET.parse(xml_path).getroot()
 
     all_annotations = root.find('Annotations').iter('Annotation')
     polygon_annotations = filter(lambda annot: annot.get('Type') == 'Polygon', all_annotations)
@@ -15,4 +15,19 @@ def read_polygons_xml(xml_path):
     return polygons
 
 def write_polygons_xml(polygons, xml_path):
-    pass
+    root = ET.Element("ASAP_Annotations")
+    anns = ET.SubElement(root, "Annotations")
+
+    [_create_subelement(anns, polygon, i) for i, polygon in enumerate(polygons)]
+
+    tree = ET.ElementTree(root)
+    tree.write(xml_path)
+
+def _create_subelement(anns, polygon, i):
+    ann = ET.SubElement(anns, "Annotation", Name=str(i), Type="Polygon")
+    coords = ET.SubElement(ann, "Coordinates")
+
+    x1, y1, x2, y2 = polygon
+    points = [(x1, y1), (x2, y1), (x1, y2), (x2, y2)]
+    [ET.SubElement(coords, "Coordinate", Order=str(idx), X=str(point[0]), Y=str(point[1])) for idx, point in
+     enumerate(points)]
