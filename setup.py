3989b2b838c5cea831418c832816c39f5369cfd9
Alexander Yaroshevich <vozman@yandex.ru>
2018-05-10 15:42:57 +0300

    create setup py
    update setup py
    
    update setup py
    
    update setup py
    
    update setup py
    
    update setup py
    
    update setup py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='slide-analysis-nn',
+    version='1',
+    install_requires=['matplotlib',
+                      'werkzeug',
+                      'keras',
+                      'tensorflow',
+                      'opencv-python',
+                      'falcon',
+                      'numpy',
+                      'h5py',
+                      'shapely ',
+                    ],
+    url='',
+    license='MIT',
+    author='vozman',
+    author_email='vozman@yandex.ru',
+    description=''
+)
