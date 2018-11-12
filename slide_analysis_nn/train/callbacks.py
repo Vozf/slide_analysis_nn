05860f0c285a9a7a1df80b084c129a1800ae4eef
Alexander Yaroshevich <vozman@yandex.ru>
2018-11-12 12:00:49 +0300

    add roc auc

--- slide_analysis_nn/train/callbacks.py
+++ slide_analysis_nn/train/callbacks.py
@@ -4,6 +4,7 @@ from os import path, unlink
 import keras
 import tensorflow
 from tensorflow.python.framework.graph_util import convert_variables_to_constants
+import tensorflow as tf
 
 
 class TB(keras.callbacks.TensorBoard):
@@ -104,3 +105,21 @@ class BestModelCheckpoint(keras.callbacks.ModelCheckpoint):
         files.sort(key=path.getmtime)
         for f in files[:-1]:
             unlink(f)
+
+
+def auc_roc(y_true, y_pred):
+    # any tensorflow metric
+    value, update_op = tf.contrib.metrics.streaming_auc(y_pred, y_true)
+
+    # find all variables created for this metric
+    metric_vars = [i for i in tf.local_variables() if 'auc_roc' in i.name.split('/')[1]]
+
+    # Add metric variables to GLOBAL_VARIABLES collection.
+    # They will be initialized for new session.
+    for v in metric_vars:
+        tf.add_to_collection(tf.GraphKeys.GLOBAL_VARIABLES, v)
+
+    # force to update metric values
+    with tf.control_dependencies([update_op]):
+        value = tf.identity(value)
+        return value
