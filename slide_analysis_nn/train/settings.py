220f63da93a2de5e8cf714fedb08c2b5d293f7f6
Alexander Yaroshevich <vozman@yandex.ru>
2018-11-12 20:14:42 +0300

    update settings for inception

--- slide_analysis_nn/train/settings.py
+++ slide_analysis_nn/train/settings.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 
+from slide_analysis_nn.tile import TILE_SIZE
+
 PROJECT_PATH = Path(os.path.abspath(os.path.dirname(__file__)))
 
 TRAIN_TEST_DATASET_PERCENT = 0.7
@@ -15,7 +17,7 @@ VALIDATION_STEPS = None
 MIN_DELTA = 1e-4
 PATIENCE = 5
 
-NETWORK_INPUT_SHAPE = (224, 224, 3)
+NETWORK_INPUT_SHAPE = (TILE_SIZE, TILE_SIZE, 3)
 
 SNAPSHOTS_DIR = PROJECT_PATH / 'snapshots'
 TF_BOARD_LOGS_DIR = PROJECT_PATH / 'logs'
