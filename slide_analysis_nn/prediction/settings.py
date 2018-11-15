3f7dd202b2265906617f3d4f153c3d3f8876f45f
Alexander Yaroshevich <vozman@yandex.ru>
2018-11-15 14:27:32 +0300

    update link for mobilenet

--- slide_analysis_nn/prediction/settings.py
+++ slide_analysis_nn/prediction/settings.py
@@ -1,5 +1,5 @@
 from slide_analysis_nn.tile import TILE_SIZE
 
 DEFAULT_SCORE_THRESHOLD = 0.98
-MODEL_DOWNLOAD_URL = 'http://bit.ly/2IbEG9B'
+MODEL_DOWNLOAD_URL = 'http://bit.ly/2PXnFTP'
 TILE_STEP = TILE_SIZE
